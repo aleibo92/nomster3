1a06df4aa2ba3a12258a3b90b44c3cc14cc756d2
aleibo92 <aleibo92@gmail.com>
2019-06-27 00:26:39 +0000

    lesson 25

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
 
   def index
     @places = Place.order(:name).page params[:page]
@@ -20,6 +20,9 @@ class PlacesController < ApplicationController
 
   def edit 
     @place = Place.find(params[:id])
+      if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+    end
   end
 
   def update

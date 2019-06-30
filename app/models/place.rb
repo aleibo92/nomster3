a99cac430bdcde23eb1260c8873c48f8691440b2
aleibo92 <aleibo92@gmail.com>
2019-06-30 20:04:04 +0000

    lesson 31

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,12 @@
 class Place < ApplicationRecord
   paginates_per 3
 
-  belongs_to(:user)
+  belongs_to :user
+  has_many :comments
+  
   geocoded_by :address
   after_validation :geocode
+
 
   validates(:address, :description, :name, {length: {minimum: 3 }, presence: true})
 
